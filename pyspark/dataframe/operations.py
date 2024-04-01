88e6309658e01d99b457a5ef693add8c698fa1f1
Mahalakshmi03935 <162538876+Mahalakshmi03935@users.noreply.github.com>
2024-04-01 22:00:13 +0530

    Update operations.py

--- pyspark/dataframe/operations.py
+++ pyspark/dataframe/operations.py
@@ -1,1 +1,31 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import Row
 
+spark = SparkSession.builder.appName("LogicalOperations").getOrCreate()
+
+
+schema = StructType([
+    StructField("Name", StringType(), True),
+    StructField("Age", IntegerType(), True)
+])
+
+
+data = [("Raja", 10),
+        ("Maha", 15),
+        ("Lakshmi", 20)]
+
+df = spark.createDataFrame(data, schema)
+print("Original DataFrame:")
+df.show()
+
+# AND 
+and_df = df.filter((df["Name"] == "Raja") & (df["Age"] < 20))
+print("DataFrame after AND operation:")
+and_df.show()
+
+
+# OR 
+or_df = df.filter((df["Name"] == "Raja") | (df["Age"] == 15))
+print("DataFrame after OR operation:")
+or_df.show()
