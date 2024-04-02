2a5f204317592ddad327ee906cbcb1f5a5861b7a
Mahalakshmi03935 <162538876+Mahalakshmi03935@users.noreply.github.com>
2024-04-02 10:24:57 +0530

    Update read_json.py

--- pyspark/dataframe/read_json.py
+++ pyspark/dataframe/read_json.py
@@ -1,1 +1,5 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("data").getOrCreate()
+df = spark.read.option("header","true").json('C:/Users/RajaMahalakshmiB/Desktop/practice/practice/pyspark/resource_file/sample.json')
+df.show()
 
