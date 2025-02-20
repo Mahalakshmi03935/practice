53fe38079272ea5a66023685359704947fdc6ccc
Mahalakshmi03935 <162538876+Mahalakshmi03935@users.noreply.github.com>
2025-02-20 23:34:43 +0530

    Delete pyspark directory

--- pyspark/dataframe/read_csv.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StringType,StructField,IntegerType,DateType
-from pyspark.sql.functions import upper,lower
-from pyspark.sql.functions import substring
-from pyspark.sql.functions import to_date
-spark = SparkSession.builder.appName("data").getOrCreate()
-df = spark.read.option("header","true").option("inferschema","true").csv('C:/Users/RajaMahalakshmiB/Desktop/practice/practice/pyspark/resource_file/customers_100.csv')
-df.show()
--- pyspark/dataframe/read_json.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("data").getOrCreate()
-df = spark.read.option("header","true").json('C:/Users/RajaMahalakshmiB/Desktop/practice/practice/pyspark/resource_file/sample.json')
-df.show()
-
